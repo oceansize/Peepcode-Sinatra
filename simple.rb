d8943066513c386618daa3be678818327b2aefb7
Roi Driscoll <roi.driscoll@gmail.com>
2014-07-30 20:19:38 +0100

    Show that @@ layout will be called as a default, whether explicitly called or not

--- simple.rb
+++ simple.rb
@@ -2,12 +2,27 @@ require 'rubygems'
 require 'sinatra'
 
 get '/' do
+  @title = "Fly Me To The Moon"
   erb :fly
 end
 
 # __END__ is a Ruby Macro which means anything that comes after is plain text - this is where our template starts.
 # @@ signifies the beginning of a template block in Sinatra
+# Using 'layout' is a keyword. The @@ layout template will not render if you call it 'monkey' because it has not been explicitly called. Layout however, is a default that will activate automatically.
+
 __END__
 
+@@ layout
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title><%= @title %></title>
+</head>
+<body>
+  <%= yield %>
+</body>
+</html>
+
 @@ fly
-Fly me to the Moon.
+Anything goes.
